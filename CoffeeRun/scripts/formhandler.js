1adda71fe9545d68a26620f6e12b0b240e7fb601
Tushar Kamble <tushark26@csu.fullerton.edu>
2017-03-13 20:29:42 -0700

    Assignment 6

---
+++ CoffeeRun/scripts/formhandler.js
@@ -0,0 +1,49 @@
+(function(window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+    //var FormHandler = App.FormHandler;
+
+    function FormHandler(selector) {
+        // Code will go here
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+        this.$formElement = $(selector);
+        if (this.$formElement.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+
+    FormHandler.prototype.addSubmitHandler = function(fn) {
+        console.log('Setting submit handler for form');
+        // More code will go here
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+            var data = {};
+            $(this).serializeArray().forEach(function(item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+                //document.getElementById("email1").innerHTML = data.emailAddress;
+            });
+            console.log(data);
+            fn(data);
+
+
+            function showMore() {
+                var x = data.emailAddress;
+                if (x != "") {
+                    document.getElementById("show").style.display = "block";
+                }
+            }
+
+            this.reset();
+            this.elements[0].focus();
+        });
+    };
+
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+})(window);
